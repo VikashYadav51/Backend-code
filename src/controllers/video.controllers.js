61496946f738775cee7637de13418dd140caab5d
Vikash <vikashyadav5131@gmail.com>
2026-01-28 11:06:37 +0530

    add video controllers.js files

---
+++ src/controllers/video.controllers.js
@@ -0,0 +1,18 @@
+import { Video } from "../models/video.models.js";
+
+import ApiResponse from "../utils/ApiResponse.js";
+
+import ApiError from "../utils/ApiError.js";
+
+import asyncHandler from "../utils/asyncHandler.js";
+
+import { Channel } from "../models/channel.models.js";
+
+
+const changeTittle = asyncHandler( async( req, res) =>{
+     
+});
+
+export {
+    changeTittle,
+}
